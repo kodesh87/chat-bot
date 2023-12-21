e078f61359c102e96b8f281a804561dfdca3c0e4
Wira N K Sihombing <kodesh87@gmail.com>
2023-12-21 11:33:42 +0700

    working RunnableSequence with LLM

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { ChatOpenAI } from 'langchain/chat_models/openai'
 import { PromptTemplate } from 'langchain/prompts'
 import { StringOutputParser } from 'langchain/schema/output_parser'
-import { RunnableSequence } from 'langchain/schema/runnable';
+import { RunnableSequence, RunnablePassthrough } from 'langchain/schema/runnable';
 
 import dotenv from 'dotenv';
 
@@ -10,27 +10,56 @@ dotenv.config();
 const openAIApiKey = process.env.OPENAI_API_KEY
 const llm = new ChatOpenAI({ openAIApiKey })
 
+// punctuation
 const punctuationTemplate = `Given a sentence, add punctuation where needed. 
     sentence: {sentence}
     sentence with punctuation:  
     `
 const punctuationPrompt = PromptTemplate.fromTemplate(punctuationTemplate)
 
+// grammar
 const grammarTemplate = `Given a sentence correct the grammar.
     sentence: {punctuated_sentence}
     sentence with correct grammar: 
     `
 const grammarPrompt = PromptTemplate.fromTemplate(grammarTemplate)
 
-const chain = RunnableSequence.from([
+// translation
+const translationTemplate = `Given a sentence, translate that sentence into {language}.
+Sentence: {grammatically_correct_sentence}.
+Translated sentence:
+`
+const translationPrompt = PromptTemplate.fromTemplate(translationTemplate)
+
+//chains
+const punctuationChain = RunnableSequence.from([
     punctuationPrompt,
     llm,
-    new StringOutputParser(),
-    { punctuated_sentence: prevResult => prevResult },
-    // prevResult => console.log(prevResult),
+    new StringOutputParser()
+]);
+const grammarChain = RunnableSequence.from([
     grammarPrompt,
     llm,
     new StringOutputParser()
+]);
+const translationChain = RunnableSequence.from([
+    translationPrompt,
+    llm,
+    new StringOutputParser()
+])
+
+// main chain
+const chain = RunnableSequence.from([
+    { 
+        punctuated_sentence: punctuationChain,
+        original_input: new RunnablePassthrough() 
+    },
+    // prevResult => console.log(prevResult),
+    { 
+        grammatically_correct_sentence: grammarChain,
+        language: ({ original_input }) => original_input.language 
+    },
+    translationChain
 ]);
 
 const response = await chain.invoke({
