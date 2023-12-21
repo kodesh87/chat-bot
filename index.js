10a6a1db0f1a687ea4d02d2b24466aa378f54621
Wira N K Sihombing <kodesh87@gmail.com>
2023-12-21 08:53:14 +0700

    add dotenv, use OpenAI Embedding and store to supabase

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import fs from 'fs/promises';
+import { createClient } from '@supabase/supabase-js';
+import { SupabaseVectorStore } from 'langchain/vectorstores/supabase';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 try {
     const filePath = '/Users/kodesh87/Developer/nodejs-fe-application/chat-bot/faq.txt';
@@ -12,9 +18,22 @@ try {
     });
 
     const output = await splitter.createDocuments([text]);
-    for (const o of output) {
-        console.log(o);
-    }
+    
+    const sbApiKey = process.env.SUPABASE_API_KEY;
+    const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT; 
+    const openAIApiKey = process.env.OPENAI_API_KEY;
+
+    const client = createClient(sbUrl, sbApiKey);
+
+    await SupabaseVectorStore.fromDocuments(
+        output,
+        new OpenAIEmbeddings({ openAIApiKey }),
+        {
+            client,
+            tableName: 'documents'
+        }
+    );
+
 } catch (err) {
     console.log(err);
 }
